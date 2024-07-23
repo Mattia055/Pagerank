2b2fba7e8efe22bd66b3660d763b60f0366f5023
Mattia055 <mattiapiras055@gmail.com>
2024-07-23 12:57:21 +0200

    added to parsed_routine

--- src/lib_graph.c
+++ src/lib_graph.c
@@ -1,6 +1,9 @@
 #include "./src/lib_graph.h"
+#include "./src/lib_supp.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 /**
  * NOTES
@@ -10,6 +13,10 @@
  * 
  * - inmap_free()
  * - inmap_push()
+ * - BUF_SIZE
+ * - DYN_DEF
+ * - MUX_DEF
+ * - THREAD_TERM
  */
 
 #define HERE __FILE__,__LINE__
@@ -86,8 +93,169 @@ void graph_destroy(graph *g){
  * from the main thread that updates the count on
  * the "out" array
  */
+void *parser_routine(void *);
+void *sorter_routine(void *);
 
 graph *graph_parse(char *pathname, int thread_count){
+    char *getline_buff = NULL;
+    size_t getline_size = 0;
+    int r,c,edges_count;
+    int lines = 0;
+
     FILE *file = xfopen(pathname,"r",HERE);
+
+    do{
+        if(getline(&getline_buff,&getline_size,file)==-1){
+            error("[getline] comments",HERE);
+        }
+        lines ++;
+    }while(getline_buff[0] == '%');
+
+    if(sscanf(getline_buff,"%d %d %d",&r,&c,&edges_count)!=3){
+        error("[sscanf] parsing first significant line",HERE);
+    }
+
+    if(r!=c || r < 1 || edges_count<0){
+        error("[graph_parse] Bad file",HERE);
+    }
+
+    graph *g = graph_alloc(r,edges_count);
+
+    /**
+     * Alloc and Init of structures needed by threads
+     */
+
+    int *dynamic_size   = xmalloc(r * sizeof(int),HERE);
+    int *pc_buffer      = xmalloc(BUF_SIZE * sizeof(int),HERE);
+
+    sem_t free_slots, data_items;
+    xsem_init(&free_slots,0,BUF_SIZE / 2,HERE);
+    xsem_init(&data_items,0,0,HERE);
+
+    pthread_mutex_t buffer_mutex;
+    xpthread_mutex_init(&buffer_mutex,HERE);
+
+    /**
+     * Uses an array of mutex to synch each thread on the corresp.
+     * struct.
+     * 
+     * The choice on which mutex to use is done using the '%' operator
+     * so that it minimizes collisions;
+     */
+    pthread_mutex_t **graph_mux = xmalloc(MUX_DEF * sizeof(pthread_mutex_t *),HERE);
+    for(int i = 0; i<MUX_DEF; i++){
+        xpthread_mutex_init(graph_mux[i],HERE);
+    }
+
+    pthread_t tid[thread_count];
+    parser_attr thread_attr;
+    thread_attr.buffer_mux  = &buffer_mutex;
+    thread_attr.graph_mux   = graph_mux;
+    thread_attr.pc_buffer   = pc_buffer;
+    thread_attr.index       = 0;
+    thread_attr.free_slots  = &free_slots;
+    thread_attr.data_items  = &data_items;
+    thread_attr.dyn_size    = dynamic_size;
+
+    for(int i = 0; i < thread_count; i++){
+        xpthread_create(&tid[i],parser_routine,&thread_attr,HERE);
+    }
+
+    //START READING EDGES FROM FILE
+
+    int ori,dest;
+    int index = 0;
+    while(getline(&getline_buff,&getline_size,file) != -1){
+        lines ++;
+
+        if(sscanf(getline_buff,"%d %d",&ori,&dest)!=2){
+            char *err_mess;
+            if(asprintf(&err_mess, "[sscanf] error parsing edge at line %d",lines)<0){
+                error("[sscanf] error parsing edge",HERE);
+            }
+            error(err_mess,HERE);
+        }
+
+        //discard not valid edges
+        if(ori == dest || ori<0 || dest <0 || ori>g->nodes || dest>g->nodes){
+            g->edges -=1;
+            continue;
+        }
+
+        //insert edge on pc_buffer
+        xsem_wait(&free_slots,HERE);
+            pc_buffer[index] = ori;
+            pc_buffer[index + 1 % BUF_SIZE] = dest;
+            index = (index + 2) % BUF_SIZE;
+        xsem_post(&data_items,HERE); 
+
+    }
+
+    //insert termination value for threads
+    xsem_wait(&free_slots,HERE);
+        pc_buffer[index] = THREAD_TERM;
+        pc_buffer[index + 1 % BUF_SIZE] = THREAD_TERM;
+        index = (index + 2) % BUF_SIZE;
+    xsem_post(&data_items,HERE); 
+
+    for(int i = 0; i<tid[thread_count]; i++){
+        xpthread_join(tid[i],NULL,HERE);
+    }
+
+    //deallocs struct needed no more
+    for(int i = 0; i<MUX_DEF; i++){
+        xpthread_mutex_destroy(graph_mux[i],HERE);
+    }
+    free(graph_mux);
+    free(dynamic_size);
+    free(getline_buff);
+    xsem_destroy(&free_slots,HERE);
+
+    //i'm gonna reuse buffer_mux and the semaphores (same for the buffer)
+    xsem_init(&free_slots,0,BUF_SIZE,HERE);
+    pthread_mutex_t index_mux;
+    xpthread_mutex_init(&index_mux,HERE);
+    
+
+    /**
+     * Create a new group of threads that work with a shared index sorting
+     * the "in" vectors and inserting duplicates in a pc_buffer that main
+     * thread reads to decrement "out[i]"
+     */
+
+}
+
+void *parser_routine(void *attr){
+    parser_attr *arg = (parser_attr *)attr;
+
+    int ori,dest;
+
+    while(true){
+        xsem_wait(arg->data_items,HERE);
+            xpthread_mutex_lock(arg->buffer_mux,HERE);
+
+                ori     = arg->pc_buffer[arg->index];
+                dest    = arg->pc_buffer[(arg->index + 1) % BUF_SIZE];
+                arg->index = (arg->index + 2) % BUF_SIZE;
+
+            xpthread_mutex_unlock(arg->buffer_mux,HERE);
+        xsem_post(arg->free_slots,HERE);
+
+        if(ori == THREAD_TERM && dest == THREAD_TERM){
+            pthread_exit(NULL);
+        }
+
+        /**
+         * insert the node ori in in[dest].
+         * Uses static inline method inmap_push
+         * 
+         * takes mutex from mutex_vector
+         */
+        xpthread_mutex_unlock(arg->graph_mux[dest % MUX_DEF],HERE);
+            inmap_push(&(arg->graph->in[dest]), ori, &(arg->dyn_size[dest]));
+            (arg->graph->out)[ori] += 1;
+        xpthread_mutex_unlock(arg->graph_mux[dest % MUX_DEF],HERE);
+
+    }
 }
 
--- src/lib_graph.h
+++ src/lib_graph.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <semaphore.h>
+
+#define HERE __FILE__,__LINE__
+#define BUF_SIZE    2048
+#define DYN_DEF     10
+#define MUX_DEF     500
+#define THREAD_TERM -1  //should be negative or bigger than the highest graph node index
 
 typedef struct{
     int *vector;
@@ -18,6 +25,32 @@ typedef struct{
     int *dead_end;      //vector of dead end nodes tag (length: dead_count)
 }graph;
 
+/**
+ * inmap_push()
+ * ------------
+ * param 
+ *      inmap **obj: pointer to inmap struct inside "in" vector
+ *      int    elem: node to push in the array
+ *      int   *size: pointer to int variable, useful to realloc the vector
+ */
+
+static inline void inmap_push(inmap **obj, int elem, int *size){
+    if(*obj == NULL){
+        *obj = xmalloc(sizeof(inmap), HERE);
+        *size = DYN_DEF;
+        (*obj)->vector = xmalloc(DYN_DEF * sizeof(int), HERE);
+        (*obj)->length = 0;
+    }
+    else if((*obj)->length == *size){
+        *size *= 2;
+        (*obj)->vector = xrealloc((*obj)->vector, (*size) * sizeof(int), HERE);
+    }
+
+    (*obj)->vector[(*obj)->length] = elem;
+    (*obj)->length += 1;
+
+}
+
 static inline void inmap_free(inmap *ptr){
     if(ptr == NULL)
         return;
@@ -27,5 +60,16 @@ static inline void inmap_free(inmap *ptr){
 }
 
 graph *graph_alloc(int nodes, int edges);
+
+typedef struct parser_attr{
+    int             index;
+    int             *pc_buffer;
+    int             *dyn_size;
+    pthread_mutex_t *buffer_mux;
+    pthread_mutex_t **graph_mux;
+    sem_t           *free_slots;
+    sem_t           *data_items;
+    graph           *graph;
+}parser_attr;
 
 #endif
