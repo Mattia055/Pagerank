dddedef0a486873cbb4d143c331391bf17791e1d
Mattia055 <mattiapiras055@gmail.com>
2024-07-29 17:03:08 +0200

    added if guards

--- pagerank.c
+++ pagerank.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <getopt.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "./src/lib_supp.h"
 #include "./src/lib_graph.h"
@@ -12,7 +13,17 @@
 
 #define _GNU_SOURCE
 
-#define HERE __FILE__,__LINE__
+#ifndef SIGNAL_STREAM
+#define SIGNAL_STREAM stderr
+#endif
+
+#ifndef INFO_STREAM
+#define INFO_STREAM stdout
+#endif
+
+#ifndef CHECK_TIME
+#define CHECK_TIME false
+#endif
 
 /**
  * global variables shared with signal handler
@@ -20,48 +31,56 @@
 double *X_previous;
 pthread_mutex_t signal_mux;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    struct timeval start,end,parse_start,parse_end,page_start,page_end;
+    /**Time measure struct */
+    xgettimeofday(&start,CHECK_TIME,HERE);
+
     int k = 3;
     int m = 100;
     double d = 0.9;
     double e = 1e-7;
     int threads = 3;
-    //turned true for testing
-    bool signal = false;
+    // stored true
+    bool signal = true;
 
     /**
      * Parameter parsing from argv
      */
     int opt;
-    while ((opt = getopt(argc, argv, "shk:m:d:e:t:")) != -1) {
-        switch (opt) {
-            case 'k':
-                k = atoi(optarg);
-                break;
-            case 'm':
-                m = atoi(optarg);
-                break;
-            case 'd':
-                d = atof(optarg);
-                break;
-            case 'e':
-                e = atof(optarg);
-                break;
-            case 't':
-                threads = atoi(optarg);
-                break;
-            case 's':
-                signal = true;
-                break;
-            case 'h':
-                printHelp();
-                exit(EXIT_SUCCESS);
-            default: /* '?' */
-                exit(EXIT_FAILURE);
+    while ((opt = getopt(argc, argv, "shk:m:d:e:t:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'k':
+            k = atoi(optarg);
+            break;
+        case 'm':
+            m = atoi(optarg);
+            break;
+        case 'd':
+            d = atof(optarg);
+            break;
+        case 'e':
+            e = atof(optarg);
+            break;
+        case 't':
+            threads = atoi(optarg);
+            break;
+        case 's':
+            signal = false;
+            break;
+        case 'h':
+            printHelp();
+            exit(EXIT_SUCCESS);
+        default: /* '?' */
+            exit(EXIT_FAILURE);
         }
     }
 
-    if(optind >= argc){
+    if (optind >= argc)
+    {
         puts("[pagerank] no input file");
         puts("usage: ./pagerank [-h] [-k K] [-m M] [-d D] [-e E] [-t T] <infile>");
         return -1;
@@ -70,38 +89,69 @@ int main(int argc, char *argv[]){
     char *infile = argv[optind];
 
     int iter_count = 0;
-    
+
+    int graph_nodes = -1;
+
+    pthread_t signal_tid;
 
     /**
      * Setup thread that handles all signals
-     * 
+     *
      * Masks signals of main thread and delegates
      * their handling (via sigwait) to an auxiliary thread;
      */
-    if(signal){
+    if (signal)
+    {
         sigset_t local_mask;
         sigfillset(&local_mask);
-        //DEBUG: create thread;
+        sig_handler_attr handler_attr;
+        handler_attr.signal_stream  = SIGNAL_STREAM;
+        handler_attr.X_previous     = &X_previous;
+        handler_attr.shared_mux     = &signal_mux;
+        handler_attr.nodes          = &graph_nodes;
+        handler_attr.iter_count     = &iter_count;
+        pthread_sigmask(SIG_SETMASK,&local_mask,NULL);
+        xpthread_create(&signal_tid,signal_handler_routine,&handler_attr,HERE);
     }
 
     /**
-     * start multithread 
+     * start multithread
      * parsing of mtx file to graph
      * ----------------------------
      * calls graph_create(char *) that returns
      * a graph struct;
      */
 
-    graph *g = graph_parse(infile,threads);
+    xgettimeofday(&parse_start,CHECK_TIME,HERE);
+    graph *g    = graph_parse(infile, threads);
+    xgettimeofday(&parse_end,CHECK_TIME,HERE);
+    graph_nodes = g->nodes;
 
-    printGraphInfo(g,stdout,false);
+    printGraphInfo(g,INFO_STREAM, false);
+
+    xgettimeofday(&page_start,CHECK_TIME,HERE);
     double *ranks = pagerank(g, d, e, m, threads, &iter_count);
+    xgettimeofday(&page_end,CHECK_TIME,HERE);
 
-    printStats(ranks,g->nodes,iter_count,m,k,stdout);
+    printStats(ranks, g->nodes, iter_count, m, k, INFO_STREAM);
 
     graph_destroy(g);
     free(ranks);
 
-    return 0;
+    if(signal){
+        pthread_kill(signal_tid,SIGUSR2);
+        xpthread_join(signal_tid,NULL,HERE);
+    }
+    xgettimeofday(&end,CHECK_TIME,HERE);
 
+    /**
+     * Print Time Stats
+     */
+    if(CHECK_TIME){
+        fprintf(stderr,"\n--------------------\nTime Stats: %s\n--------------------\n",infile);
+        fprintf(stderr,"parsing\ttime\t\t%.6f sec\n",exctract_time(parse_start,parse_end,CHECK_TIME));
+        fprintf(stderr,"compute\ttime\t\t%.6f sec\n",exctract_time(page_start,page_end,CHECK_TIME));
+        fprintf(stderr,"total\ttime\t\t%.6f sec\n",exctract_time(start,end,CHECK_TIME));
+    }
+    return 0;
 }
